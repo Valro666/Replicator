ed82c69d1ce62dea0425284faff8491e23679594
Valerian Damm <damm2u@fst-ats-240-l02.fst.site.univ-lorraine.fr>
2019-02-11 21:31:04 +0100

    apti cube

--- replicator.cpp
+++ replicator.cpp
@@ -18,7 +18,7 @@ class point {
 	x=0.0;
 	y=0.0;
 	};
-	go(point a ){
+	void go(point a ){
 	x=a.x;
 	y=a.y;
 	}
@@ -37,7 +37,7 @@ class point {
 	return res;
 	}
 	
-	rotate(point centre, double angle ){
+	void rotate(point centre, double angle ){
     angle *= M_PI / 180.0;
     double xM = x - centre.x;
     double yM = y - centre.y;
@@ -45,15 +45,29 @@ class point {
     y = - xM * sin(angle) + yM * cos(angle) + centre.y;
 	}
 	
-	translate(double tx,double ty ){
+	void translate(double tx,double ty ){
     x = x + tx;
     y = y + ty;
+	}
+	
+    bool equals(point b){
+    if(b.x==x){
+        if(b.y==y){
+            return true;
+        } 
+    } 
+    return true;
 	}
 	
 	point jump(double jx,double jy ){
 	point t(x,y) ;
     t.x = t.x + jx;
     t.y = t.y + jy;
+	return t;
+	}
+	
+    point duplicate(){
+	point t(x,y) ;
 	return t;
 	}
 
@@ -64,9 +78,76 @@ class shape {
 	vector<point> vertices;
 	
 	void inverse();
+    
 	void add(point a){
 		vertices.push_back(a);
 	}
+	
+	void rotate(point centre, double angle ){
+        for(int i = 0 ;i < vertices.size();i++){
+            vertices[i].rotate(centre,angle);
+        }
+    };
+    
+    void translate(double tx,double ty ){
+        for(int i = 0 ;i < vertices.size();i++){
+            vertices[i].translate(tx,ty);
+        }
+    };
+    
+    shape duplicate(){
+        shape s;
+        for(int i = 0 ;i < vertices.size();i++){
+            s.add(vertices[i].duplicate());
+        }
+    };
+    
+    void melt(point a){
+        shape s;
+        for(int i = 0 ;i < vertices.size();i++){
+        //    printf("molp");
+            if(!vertices[0].equals(a)){
+                point a = vertices[0].duplicate();
+                vertices.erase(vertices.begin(),vertices.begin()+1);
+                vertices.push_back(a);
+            }
+        }
+      //  printf("mmpolohtcxd");
+    };
+    
+    void fusion(shape s){
+        
+        
+        
+        point p =s.vertices[0];
+        double d = vertices[0].dist(s.vertices[0]) ;
+        //for(int i = 0 ;i < vertices.size();i++){
+                    for(int j = 0 ;j < s.vertices.size();j++){
+    //                    printf("azerty");
+                        if(d>vertices[vertices.size()].dist(s.vertices[j])){
+                            d = vertices[vertices.size()].dist(s.vertices[j]) ;
+                            p = s.vertices[j];
+                        }
+                }
+  //              printf("nji");
+        s.melt(p);
+//        printf("fghjklm");
+        
+                    for(int j = 0 ;j < s.vertices.size();j++){
+                        //printf("azertyyyyyyyyyyyyy");
+                        vertices.push_back(s.vertices[j]);
+                        //printf("azeeeeeeeeeeeeeeeee");
+                        }     
+        //}
+    };
+    
+    void dellast(){
+        vertices.pop_back();
+    };
+    
+    int size(){
+        return vertices.size();
+    };
 };
 
 point tete;
@@ -104,96 +185,66 @@ void line(point a , point b){
 	file<<"G92 E0.0"<<endl;
 }
 
-void squarefill(){
-	vector<point> poly ;
-	poly.push_back(point(50,50));
-	poly.push_back(point(50,60));
-	poly.push_back(point(60,60));
-	poly.push_back(point(60,50));
+shape squarefill(){
 	double minx ;
 	double maxx ;
 	double miny ;
 	double maxy ;
-	double y = 50;
-	vector<point> fill ;
-	point p(50,50);
+	//double y = 50;
+	shape fill ;
+
+	
 	double m = 0;
-	fill.push_back(p);
+	//fill.add(p);
 	int tour = 0 ;
-	while(y<=60){
-		//point tmp;
+    
+    double pour = 5;
+    double bid = 0.2;
+    //double nn = extrud(bid)*M_PI*bid*bid*0.25;
+    double nn = 0.5;
+    point p(50,50);
+    //p.translate(0.0,0.2);
+    double y = 50;
+    p.translate(0.0,nn);
+    fill.add(p.duplicate());
+	while(y<60-nn){
+
+        point t = p.duplicate();
 		if(tour%2==1){
-			point l =p.jump(0,nw);
-			point m =l.jump(-10,0);
-			fill.push_back(l);
-			fill.push_back(m);
-			p = m.jump(0,0);
+            t = p.duplicate();
+            t.translate(-(10.0-nn-nn),0.0);
+            //p = m.jump(0,0);
 			}
 		else{
-			point l =p.jump(0,nw);
-			point m =l.jump(10,0);
-			fill.push_back(l);
-			fill.push_back(m);
-			p = m.jump(0,0);
+            t = p.duplicate();
+            t.translate(10.0-nn-nn,0.0);
+			//p = m.jump(0,0);
 			}
-		
-		fill.push_back(p);
+        fill.add(t);
+        p = t.duplicate();
+		//fill.add(p);
+        p.translate(0.0,nn);
+        fill.add(p.duplicate());
 		tour = (tour +1)%2;
-		y = y+nw;
+		y = y+nn;
 	}
 	
-	polygnon(poly);
-	polygnon(fill);
+	//fill.dellast();
+	fill.translate(nn,0.0);
+	return fill;
+	//polygnon(poly);
+	//polygnon(fill);
 	
 }
 
-void squarefill2(){
-	vector<point> poly ;
-	poly.push_back(point(50,50));
-	poly.push_back(point(50,60));
-	poly.push_back(point(60,60));
-	poly.push_back(point(60,50));
-	double minx ;
-	double maxx ;
-	double miny ;
-	double maxy ;
-	double y = 50;
-	vector<point> fill ;
-	point p(50,50);
-	double m = 0;
-	fill.push_back(p);
-	int tour = 0 ;
-	while(y<=60){
-		//point tmp;
-		if(tour%2==1){
-			point l =p.jump(0,0.2);
-			point m =l.jump(-10,0);
-			fill.push_back(l);
-			fill.push_back(m);
-			p = m.jump(0,0);
-			}
-		else{
-			point l =p.jump(0,0.2);
-			point m =l.jump(10,0);
-			fill.push_back(l);
-			fill.push_back(m);
-			p = m.jump(0,0);
-			}
-		
-		
-		//fill.push_back(p);
-		tour = (tour +1)%2;
-		y = y+0.2;
-	}
-	
-	for(int i = 0 ; i < fill.size(); i++){
-		fill[i].rotate(point(50,50),90);
-		fill[i].translate(0,10);
-	}
-	
-	polygnon(poly);
-	polygnon(fill);
-	
+shape square(){
+	shape poly ;
+	poly.add(point(50,50));
+	poly.add(point(50,60));
+	poly.add(point(60,60));
+	poly.add(point(60,50));
+    poly.add(point(50,50));
+    return poly;
 }
 
 //G2 X Y I J E : XY centre IJ bord
@@ -217,7 +268,7 @@ void cercledelenfer(point centre , double bord){
 	
 	double x = centre.x+bord;
 	
-	move(bord);
+	//move(bord);
 	
 	for(double i = 0.0; i < 360 ; i = i+0.1){
 		point p = centre.jump(0.0,0.0);
@@ -248,9 +299,9 @@ void polygnon(shape s){
 	double tmp = 0;
 	vector<point> poly = s.vertices;
 	move(poly[0]);
-	for(i = 0 ; i < poly.size(); i++){
-		tmp = (i+1)%poly.size();
-		line(poly[i],poly[tmp]);
+	for(i = 1 ; i < poly.size(); i++){
+		tmp = (i-1);
+		line(poly[tmp],poly[i]);
 	//	cout << i<<endl;
 	}
 }
@@ -268,56 +319,37 @@ srand (time(NULL));
 	file<<"G90"<<endl;
 	file<<"G28"<<endl;
 	
-	//move(point(50,50));
-	//move(point(60,60));
-	//line(point(50,50),point(60,60));
-	
-	// rand() % 10 + 1; rand(50,60)
-	//vector<point> poly {point(50,50),point(50,60),point(60,60),point(60,50)};
-	//{point(rand(50,60),rand(50,60)),point(rand(50,60),rand(50,60)),point(rand(50,60),rand(50,60)),point(rand(50,60),rand(50,60))};
-	//for(int i = 0 ; i < 5 ; i++)
-		//poly.push_back(point(rand(50,60),rand(50,60)));
-		/*
-	poly.push_back(point(50,50));
-	poly.push_back(point(55,60));
-	poly.push_back(point(60,50));
-	poly.push_back(point(50,55));
-	poly.push_back(point(60,55));
-	poly.push_back(point(50,50));	
-	*/
-	vector<point> poly ;
-	poly.push_back(point(50,50));
-	poly.push_back(point(50,60));
-	poly.push_back(point(60,60));
-	poly.push_back(point(60,50));
-	
-	for(int i = 0; i < 4 ; i++){
-		poly[i].rotate(point(50,50),0);
-	}
-	
-	//poly
-	//squarefill();
+    shape s = square();
+    shape f = squarefill();
+    
+   // printf("fghjkl");
+    
+    s.fusion(f);
+
 	int tmp = 0 ;
-	//cercledelenfer(point(50.0,50.0),10.0);
 	for(double i = 0 ; i < 50 ;i++){
 
 		
 		z = i*taux;
-		//cercle(point(50.0,50.0),10.0);
-		//cercle(point(50.0,50.0),10.0);
-		//cercledelenfer(point(50.0,50.0),10.0);
-		//polygnon(poly);
-		if(tmp == 0){
-		squarefill();
+
+        polygnon(s);
+		/*if(tmp == 0){
+		//squarefill();
+            //polygnon(f);
 		}else{
-			
-		squarefill2();
-		}
+			//polygnon(f);
+		//squarefill2();
+		}*/
+        
+        
+        
+		//polygnon(f);
+        s.rotate(point(50.0,50.0),90.0);
+        s.translate(0.0,10.0);//*/
 		tmp = (tmp +1)%2;
 	}
 	//code
-	//line(point(50,50),point(60,60));
-	//fin code
+
 	
 	
 	file.close();

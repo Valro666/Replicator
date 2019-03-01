4eaf3b8157d0d9141364732097e0a3f606c1a0f6
Valro666 <valerian266@gmail.fr>
2019-03-01 18:41:18 +0100

    hemisphere avec detection point dans forme convexe non opti

--- replicator.cpp
+++ replicator.cpp
@@ -5,6 +5,12 @@
 #include <stdio.h>      
 #include <stdlib.h>     
 #include <time.h>  
+#include <limits>
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
 using namespace std; 
 class point {
   public :
@@ -51,12 +57,10 @@ class point {
 	}
 	
     bool equals(point b){
-    if(b.x==x){
-        if(b.y==y){
-            return true;
-        } 
+    if(b.x==x&&b.y==y){
+        return true;
     } 
-    return true;
+    return false;
 	}
 	
 	point jump(double jx,double jy ){
@@ -120,6 +124,103 @@ class shape {
             }
         }
       //  printf("mmpolohtcxd");
+	};
+
+	point minX(){
+		point p = vertices[0];
+	        for(int i = 0 ;i < vertices.size();i++){
+	            if(vertices[i].x<p.x){
+	                p = vertices[i];
+	            }
+        }
+	return p;
+	}
+	point maxX(){
+		point p = vertices[0];
+	        for(int i = 0 ;i < vertices.size();i++){
+	            if(vertices[i].x>p.x){
+	                p = vertices[i];
+	            }
+        }
+	return p;
+	}
+		point minY(){
+		point p = vertices[0];
+	        for(int i = 0 ;i < vertices.size();i++){
+	            if(vertices[i].y<p.y){
+	                p = vertices[i];
+	            }
+        }
+	return p;
+	}
+	point maxY(){
+		point p = vertices[0];
+	        for(int i = 0 ;i < vertices.size();i++){
+	            if(vertices[i].y>p.y){
+	                p = vertices[i];
+	            }
+        }
+	return p;
+	}
+	 bool inside(point p){
+		 
+		 vector<double> coef ;
+		 int i = 0 ;
+		 
+		 point xmin = minX();
+		 point xmax = maxX();
+
+		 point p1 = vertices[0];
+		 point p2 = vertices[1];
+		 point p3 = vertices[2];
+		 int tr = 1 ;
+		 point init = vertices[1];
+
+		 int s1 = 0 ;	 		 
+		 int s2 = 1 ;
+ 		 int s3 = 2 ;
+	
+	// cross product
+	
+		 while(tr > 0){
+	double denominator= ((p2.y - p3.y)*(p1.x - p3.x) + (p3.x - p2.x)*(p1.y - p3.y));
+ 	double a = ((p2.y - p3.y)*(p.x - p3.x) + (p3.x - p2.x)*(p.y - p3.y)) / denominator;
+ 	double b = ((p3.y - p1.y)*(p.x - p3.x) + (p1.x - p3.x)*(p.y - p3.y)) / denominator;
+ 	double c = 1 - a - b;
+
+	if(a>0)
+		if(a<1)
+			if(b>0)
+				if(b<1)
+					if(c>0)
+						if(c<1) return true;//*/
+
+	s3 +=1;
+	if(s3 > size()-1){
+		s3 = 0;
+		s2+=1;  
+	}
+	if(s2 > size()-1){
+		s2 = 0;
+		s1++;
+	}
+
+	p1 = vertices[s1];
+	p2 = vertices[s2];
+	p3 = vertices[s3];
+
+	if(s1==s2){
+		if(s1==s3){
+			tr = -1;
+		}
+	}
+
+	//printf("%d %d %d %d %d %d",s1,s2,s3,a,b,c);
+				}
+
+	return false;
+		 
+	 
     };
     
     void fusion(shape s){
@@ -158,17 +259,17 @@ class shape {
 };
 
 point tete;
-double z;
+double z;  //hauteur tete
 ofstream file;
-double nw = 0.4;
-double taux = 0.2;
-double dia = 1.75;
-double nn = 0.5;
+double nw = 0.4; //diametre sortie
+double taux = 0.2;  // layer
+double dia = 1.75;  //
+double nn = 0.5; //
 
 void polygnon(vector<point> poly);
 void polygnon(shape s);
-
-
+void endFunc();
+void header();
 double extrud(double dist){
 	
 	double vt = nw * taux * dist;
@@ -245,7 +346,7 @@ shape square(){
 shape cercledelenfer(point centre , double bord, double nb){
 	
     shape s ;
-	vector<point> poly ;
+	//vector<point> poly ;
 
 	
 	double diametre = 2.0*M_PI*(bord);
@@ -268,12 +369,27 @@ shape cercledelenfer(point centre , double bord, double nb){
 	point p = centre.jump(0.0,0.0);
 		p.translate(bord,0.0);
 		p.rotate(centre,360);
+
+	if(!s.vertices[0].equals(s.vertices[s.size()]))
+		s.add(s.vertices[0].duplicate());
         //s.add(p);
 	//	poly.push_back(p);
 	
 	//polygnon(poly);
 	
 	//file<<"G92 E0.0"<<endl;
+    
+    return s;
+}
+shape formconv(){
+    shape s ;
+   
+   s.add(point(50,50));
+   s.add(point(50,70));
+   s.add(point(80,100));
+   s.add(point(100,100));
+   s.add(point(100,80));
+   s.add(point(50,50));
     
     return s;
 }
@@ -335,14 +451,13 @@ void polygnon(shape s){
 	//file<<"G92 E0.0"<<endl;
 }
 
+
 void cube(){
 		file.open("cube.gcode");
 	tete.x = 0;
 	tete.y = 0;
 	//header
-	file<<"G21"<<endl;
-	file<<"G90"<<endl;
-	file<<"G28"<<endl;
+header();
 	
     shape s = square();
     shape f = squarefill();
@@ -362,22 +477,19 @@ void cube(){
         s.translate(0.0,10.0);//*/
 		tmp = (tmp +1)%2;
 	}
-	end();
 	file.close();
 }
 
 void cylindre(){
 		file.open("cylindre.gcode");
+		header();
 	tete.x = 0;
 	tete.y = 0;
 	//header
-	file<<"G21"<<endl;
-	file<<"G90"<<endl;
-	file<<"G28"<<endl;
 
-    double discret = 180 ;
-    shape vint = cercledelenferfill(point(50,50),10,discret,0);
-    shape vext = cercledelenferfill(point(50,50),10,discret,1);
+    double discret = 30 ;
+    shape vint = cercledelenferfill(point(50,50),4,discret,0);
+    shape vext = cercledelenferfill(point(50,50),4,discret,1);
 
 	int tmp = 0 ;
 	double taille = 10/taux;
@@ -392,16 +504,152 @@ void cylindre(){
 		}
 		tmp = (tmp +1)%2;
 	}
-	end();
+	endFunc();
 	file.close();
 }
 
-void end(){
+shape fillShapeX(shape sh){
+	shape s ;
 	
+	point minX =  sh.minX();
+	point maxX = sh.maxX();
+	point maxY = sh.maxY();
+	point minY = sh.minY();
+	point first = point(100,100);
+	point last = point(100,100);
+	int tr = 0 ;
+
+	for(double y = minY.y; y < maxY.y; y = y + nw  ){
+		bool start = true;
+		if((tr%2)==0){
+			for(double x = minX.x-1; x < maxX.x+1; x = x + 0.01  ){
+					if(start){
+						if(sh.inside(point(x,y))){
+						first = point(x+nw,y);
+						start = false;
+					    }
+					}
+					if(sh.inside(point(x,y))){
+						last = point(x-nw,y);
+					}
+			}
+		}else{
+			for(double x = maxX.x+1; x > minX.x-1; x = x - 0.01  ){
+					if(start){
+						if(sh.inside(point(x,y))){
+						first = point(x-nw,y);
+						start = false;
+					    }
+					}
+					if(sh.inside(point(x,y))){
+						last = point(x+nw,y);
+					}
+			}
+		}
+		if(!first.equals(last)){
+		s.add(first);
+		s.add(last);
+		}
+		tr ++;
+	}
+
+
+
+
+	return s;
+}
+
+void tricercle(double rayon=20){
+	shape s = cercledelenfer(point(50,50),rayon,8);
+	//shape s = formconv();
+	shape ss = fillShapeX(s);
+	//s.concat(ss);
+	//shape melt = s.fusion(ss);
+	polygnon(s);
+	polygnon(ss);
+	//polygnon(s);
+	//polygnon(ss);
+}
+
+void hemiv(){
 	
-	z = z + 150;
+	file.open("hemi.gcode");
+	header();
+	tete.x = 0;
+	tete.y = 0;
+	//header
+	file<<"G21"<<endl;
+	file<<"G90"<<endl;
+	file<<"G28"<<endl;
+	//head();
+	double rayon = 20;
+	double taille = 20;
+
+	// x*x + y*y = 1
+	//x*x = 1 - y*y
+	// x = sqrt(1-y*y)
+		for(double i = 0 ; i < taille ; i = i + taux){
+
+		//z = i*taux;
+			z = i;
+		//double r = ((taille - (taille - z))*rayon)/taille; 
+		double comp =  i;
+
+		double r = sqrt((rayon*rayon)-(comp*comp));
+		if(r>0)
+			tricercle(r);
+
+	printf("%f %f \r", taille, z);
+	}
+	endFunc();
+}
+
+void pyramide(){
 	
+	file.open("hemi.gcode");
+	header();
+	tete.x = 0;
+	tete.y = 0;
+	//header
+	file<<"G21"<<endl;
+	file<<"G90"<<endl;
+	file<<"G28"<<endl;
+	//head();
+	double rayon = 20;
+	double taille = 20;
+		for(double i = 0 ; i < taille ; i = i + taux){
+
+		//z = i*taux;
+			z = i;
+		//double r = ((taille - (taille - z))*rayon)/taille; 
+		double comp = taille - i;
+
+		double r = (taille-comp) * rayon; 
+		r /= taille;
+		if(rayon-r>0)
+			tricercle(rayon-r);
+
+	printf("%f %f \r", taille, z);
+	}
+	endFunc();
+}
+
+void header(){
+	
+	file<<"G21\nG90\nG28\nM140 S60\nM105\nM190 S60\nM104 S200\nM105\nM109 S200\nG92 E0\nG1 E10 F1200\nG92 E0\n"<<endl;
+	
+	move(point(-10,75));
+	move(point(75,75));
+	
+}
+
+void endFunc(){
+	
+	z = z + 15;
+	
+	move(tete);
 	move(point(0,0));
+	
 }
 
 
@@ -409,16 +657,23 @@ void end(){
 int main(int argc, char *argv[]){
 	
 	switch(atoi(argv[1])){
+		
 		case 1:
-			printf("creation d un cube dans cube.square");
+			printf("creation d un cube dans cube.gcode");
 			cube();
 			printf("fin");
 			return 1 ;
 		case 2:
-			printf("creation d un cylindre dans cylindre.square");
+			printf("creation d un cylindre dans cylindre.gcode");
 			cylindre();
 			printf("fin");
 			return 2 ;
+		break;
+		case 3:
+			printf("creation d un cylindre dans hemi.gcode");
+			hemiv();
+			printf("fin");
+			return 3 ;
 		break;
 		default : 
 		printf("cette operation n existe pas'");
